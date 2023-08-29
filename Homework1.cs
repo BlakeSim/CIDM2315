4729a4ab20c6cb6aa49d5038e9f27f2765ecf266
BlakeSim <143015314+BlakeSim@users.noreply.github.com>
2023-08-29 15:47:28 -0500

    Add files via upload

---
+++ Homework1.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.IO.Compression;
+
+public class Program
+{
+    static void Main(string[]args)
+    {
+        double x = 2.5;
+        double y = 3.3;
+        double result = 4*x*x+3*y;
+        Console.WriteLine("Z =" +result);
+    }
+}
